2f18db8a9848ef56a1b2df1ff2dd6778db2f30dc
lasi23 <lsilvere39@gmail.com>
2026-05-05 10:11:17 +0200

    fix: partie03

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,40 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+const TODOS = [
+    { todo : "conquerir le monde",
+      date : "24/11/2026"
+    },
+    { todo : "obtenir mon stage",
+      date : "29/12/2026"
+    },
+    { todo : "retourner au mexique",
+      date : "01/01/2027"
+    }
+  ]
+
+
+
 function ToDo({ todo, date }) {  
   return <li>{todo} {date}</li> 
 }
 
 function App() {
   const DATE = new Date();
+  const LIST_TODO = []
+
+  TODOS.forEach((todo, index)=> {
+    LIST_TODO.push(<ToDo key={index} todo={todo.todo} date={todo.date}/>)
+  })
   
-  return <>
+  return <> 
   <h1>Nouvelle ToDO</h1>
   <h2>{DATE.toLocaleString()}</h2>
     <ul>
-      <ToDo todo={'faire la vaisselle' } date={"24/11/2026"}/>
-      <ToDo todo={'faire la révolution' } date={"24/10/2026"}/>
-      <ToDo todo={'faire la sieste' } date={"24/12/2026"}/>
+      
+      {LIST_TODO}
     </ul>
+    
   </> 
 }
 
