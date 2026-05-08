4efae6ba717bfb9e17bba11948c88e63d7a286d5
lasi23 <lsilvere39@gmail.com>
2026-05-08 21:50:19 +0200

    feat: partie04

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,39 @@ import './App.css'
 
 const TODOS = [
     { todo : "conquerir le monde",
-      date : "24/11/2026"
+      date : "24/11/2026",
+      checked : true
     },
     { todo : "obtenir mon stage",
-      date : "29/12/2026"
+      date : "29/12/2026",
+      checked : true
     },
     { todo : "retourner au mexique",
-      date : "01/01/2027"
+      date : "01/01/2027",
+      checked : false
     }
   ]
 
 
 
-function ToDo({ todo, date }) {  
-  return <li>{todo} {date}</li> 
+function ToDo({ todo, date, checked}) {  
+  if(checked){
+    return <li className="green"><input type="checkbox" defaultChecked/>{todo} {date} </li> 
+  }
+  return <li className="orange">{todo} {date}</li>
+}
+
+function ToDoTernaire({todo, date, checked}) {
+  return <li className={checked ? "green" : 'orange'} ><input type='checkbox' checked={checked} defaultChecked/>{todo} {date} </li> 
+}
+
+function ToDoAnd({todo, date, checked}){
+  return <li>
+    {todo} {date}
+    {checked && (
+      <input type="checkbox" defaultChecked />
+    )}   
+  </li>
 }
 
 function App() {
@@ -27,7 +46,7 @@ function App() {
   const LIST_TODO = []
 
   TODOS.forEach((todo, index)=> {
-    LIST_TODO.push(<ToDo key={index} todo={todo.todo} date={todo.date}/>)
+    LIST_TODO.push(<ToDoAnd key={index} todo={todo.todo} date={todo.date} checked={todo.checked}/>)
   })
   
   return <> 
